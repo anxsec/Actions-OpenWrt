a34f6c866458cd7ff3387d686a7decebf113b388
Anx <anxsec@gmail.com>
2021-01-31 14:03:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add OpenClash Package
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
